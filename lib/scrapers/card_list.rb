8ea84e64a1e9a5b9cac237c542722dc474fd1f56
James Thompson <james@thomps.onl>
2025-02-25 16:56:57 -0500

    Refactor scraping and result handling.

--- lib/scrapers/card_list.rb
+++ lib/scrapers/card_list.rb
@@ -3,20 +3,49 @@ require "oj"
 
 module Scrapers
   class CardList
-    def initialize(connection)
+    def initialize(connection = nil)
       @connection = connection || new_connection
     end
 
-    def scrape
-      response = @connection.get(path, default_params)
-      Enumerator.new do |yielder|
-        Oj.load(response.body)["data"].each do |card_detail|
-          yielder << card_detail
-        end
+    def results
+      @results ||= Set.new
+    end
+
+    def scrape!
+      page = fetch_page
+
+      page["meta"]["pagination"]["total"]
+      additional_pages = page["meta"]["pagination"]["pageCount"].to_i - 1
+
+      extract_card_data(page["data"])
+
+      additional_pages.times do |counter|
+        page_number = counter + 2
+        page = fetch_page(page_number)
+
+        extract_card_data(page["data"])
       end
     end
 
     private
+
+    def extract_card_data(data)
+      data.each do |card_detail|
+        results.add card_detail
+      end
+    end
+
+    def fetch_page(page_number = 1)
+      params = default_params.merge({
+        pagination: {
+          page: page_number,
+          pageSize: 50
+        }
+      })
+
+      response = @connection.get(path, params)
+      Oj.load(response.body)
+    end
 
     def new_connection
       Faraday.new("https://admin.starwarsunlimited.com")
@@ -32,13 +61,9 @@ module Scrapers
         filters: {
           variantOf: {
             id: {
-              "$null": true
+              "$null" => true
             }
           }
-        },
-        pagination: {
-          page: 1,
-          pageSize: 50
         }
       }
     end
--- spec/scrapers/card_list_spec.rb
+++ spec/scrapers/card_list_spec.rb
@@ -3,43 +3,45 @@ require "scrapers/card_list"
 
 RSpec.describe Scrapers::CardList do
   subject(:scraper) { described_class.new(connection) }
-  let(:result) { scraper.scrape }
-  let(:stubs) { Faraday::Adapter::Test::Stubs.new }
+  let(:results) { scraper.results }
+  let(:stubs) { Faraday::Adapter::Test::Stubs.new(strict_mode: true) }
   let(:connection) { Faraday.new { |b| b.adapter(:test, stubs) } }
 
   before do
-    stubs.get("/api/card-list") do |env|
-      expect(env.params).to eq({
-        "locale" => "en",
-        "filters" => {
-          "variantOf" => {
-            "id" => {
-              "$null" => "true"
-            }
-          }
-        },
-        "pagination" => {
-          "page" => "1",
-          "pageSize" => "50"
-        }
-      })
-
+    stubs.get("/api/card-list?locale=en&filters[variantOf][id][$null]=true&pagination[page]=1&pagination[pageSize]=50") do |env|
       [
         200,
         {"Content-Type": "application/javascript"},
         File.read("spec/fixtures/card_list_page_1.json")
       ]
     end
+
+    stubs.get("/api/card-list?locale=en&filters[variantOf][id][$null]=true&pagination[page]=2&pagination[pageSize]=50") do |_env|
+      [
+        200,
+        {"Content-Type": "application/javascript"},
+        File.read("spec/fixtures/card_list_page_2.json")
+      ]
+    end
   end
 
-  describe "#scrape" do
-    it "fetches /api/card-list with expected parameters" do
-      scraper.scrape
-      stubs.verify_stubbed_calls
-    end
+  after do
+    Faraday.default_connection = nil
+  end
 
-    it "returns an enumerator of cards" do
-      expect(result).to be_an(Enumerator).and all(be_a(Hash))
+  describe "#scrape!" do
+    it "populates #results with card details" do
+      expect {
+        scraper.scrape!
+      }.to change {
+        scraper.results.size
+      }.from(0).to(100)
+
+      expect(results).to be_a(Set).and(all(be_a Hash))
+      expect(results).to include(hash_including({"id" => 5, "attributes" => hash_including({"title" => "Luke Skywalker"})}))
+      expect(results).to include(hash_including({"id" => 344, "attributes" => hash_including({"title" => "Colonel Yularen"})}))
+
+      stubs.verify_stubbed_calls
     end
   end
 end
