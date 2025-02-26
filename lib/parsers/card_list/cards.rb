08ef450a11cec7cb513c1a5ad5bbca3f70d09c7c
James Thompson <james@thomps.onl>
2025-02-26 16:42:33 -0500

    Add Card parser for basic info.

---
+++ lib/parsers/card_list/cards.rb
@@ -0,0 +1,46 @@
+require "oj"
+require "card"
+
+module Parsers
+  module CardList
+    class Cards
+      include Enumerable
+
+      def each
+        Dir["#{cache_dir}*.json"].each do |filename|
+          yield parse_to_card(filename)
+        end
+      end
+
+      def self.load_cache(cache_dir)
+        parser = new
+        parser.load_cache cache_dir
+        parser
+      end
+
+      def load_cache(cache_dir)
+        @cache_dir = cache_dir
+      end
+
+      def size
+        Dir["#{cache_dir}*.json"].count
+      end
+
+      private
+
+      attr_reader :cache_dir
+
+      def parse_to_card(filename)
+        json = Oj.load_file(filename)
+        ::Card.new(
+          title: json["attributes"]["title"],
+          subtitle: json["attributes"]["subtitle"],
+          card_number: json["attributes"]["cardNumber"],
+          card_id: json["attributes"]["cardId"] || json["attributes"]["cardUid"],
+          locale: json["attributes"]["locale"],
+          artist: json["attributes"]["artist"]
+        )
+      end
+    end
+  end
+end
