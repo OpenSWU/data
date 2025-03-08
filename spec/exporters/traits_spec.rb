03436af8187265022e184a8c684f302f7d5cae41
James Thompson <james@thomps.onl>
2025-03-08 13:43:03 -0500

    Refactor Traits exporter.

---
+++ spec/exporters/traits_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+require "exporters/traits"
+
+RSpec.describe Exporters::Traits do
+  subject(:exporter) { Exporters::Traits.new(cache_dir, export_dir) }
+  let(:cache_dir) { "spec/fixtures/cache/" }
+  let(:export_dir) { "tmp/tests/exports" }
+  let(:csv_path) { File.join(export_dir, "traits.csv") }
+
+  after do
+    FileUtils.rm_rf(export_dir) if Dir.exist?(export_dir)
+  end
+
+  describe "#export!" do
+    it "creates an traits.csv file in the specified export location" do
+      expect { exporter.export! }.to change {
+        File.exist?(export_dir + "/traits.csv")
+      }.from(false).to(true)
+    end
+
+    describe "the exporterd traits.csv file" do
+      let(:headers) { CSV.open(csv_path, &:readline) }
+      let(:table) { CSV.read(csv_path, headers: true) }
+
+      before do
+        exporter.export!
+      end
+
+      it "has the correct headers" do
+        expect(headers).to eq %w[id name description locale]
+      end
+
+      it "contains the expected records" do
+        expected_row = ["a7a43365-14d5-5923-be5d-487dc6ad500a", "Rebel", "", "en"]
+        expect(table.size).to eq 5
+        expect(table[0]).to eq CSV::Row.new(headers, expected_row)
+      end
+    end
+  end
+end
