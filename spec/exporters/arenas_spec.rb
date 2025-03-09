8d2a4e1d5d748731f1802a6a7b183266c56161c6
James Thompson <james@thomps.onl>
2025-03-08 19:24:23 -0500

    Improve branch coverage for exporters.

--- spec/exporters/arenas_spec.rb
+++ spec/exporters/arenas_spec.rb
@@ -19,6 +19,18 @@ RSpec.describe Exporters::Arenas do
       }.from(false).to(true)
     end
 
+    context "when the export directory already exists" do
+      before do
+        FileUtils.mkdir_p(export_dir)
+      end
+
+      it "exports as expected" do
+        expect { exporter.export! }.to change {
+          File.exist?(export_dir + "/arenas.csv")
+        }.from(false).to(true)
+      end
+    end
+
     describe "the exporterd arenas.csv file" do
       let(:headers) { CSV.open(csv_path, &:readline) }
       let(:table) { CSV.read(csv_path, headers: true) }
--- spec/exporters/expansions_spec.rb
+++ spec/exporters/expansions_spec.rb
@@ -19,6 +19,18 @@ RSpec.describe Exporters::Expansions do
       }.from(false).to(true)
     end
 
+    context "when the export directory already exists" do
+      before do
+        FileUtils.mkdir_p(export_dir)
+      end
+
+      it "exports as expected" do
+        expect { exporter.export! }.to change {
+          File.exist?(export_dir + "/expansions.csv")
+        }.from(false).to(true)
+      end
+    end
+
     describe "the exporterd expansions.csv file" do
       let(:headers) { CSV.open(csv_path, &:readline) }
       let(:table) { CSV.read(csv_path, headers: true) }
